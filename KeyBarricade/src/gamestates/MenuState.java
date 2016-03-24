cf76209df0d674d972e3d25c5d4d2cce8d1b5995
Matthias Aarnoutse <m.aarnoutse117@gmail.com>
2016-03-24 12:32:59 +0100

    Design

--- KeyBarricade/src/gamestates/MenuState.java
+++ KeyBarricade/src/gamestates/MenuState.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
-import java.awt.image.ImageObserver;
 import main.Game;
 
 public class MenuState extends GameState {
