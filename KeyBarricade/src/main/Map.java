2e035724254e99607158b71008767e886fee6b1b
Matthias Aarnoutse <15044688@student.hhs.nl>
2016-03-26 15:22:06 +0100

    Niet de blocksize meegegeven aan width, waardoor voor elke pixel een tegel werd gerendered.

--- KeyBarricade/src/main/Map.java
+++ KeyBarricade/src/main/Map.java
@@ -3,25 +3,26 @@ package main;
 import blocks.Ground;
 import blocks.Tile;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 public class Map {
 
     private int width, height;
     private Player player;
-    // private ArrayList<Tile> tiles;
-    private Tile[][] tiles2D;
+    private ArrayList<Tile> tiles;
+    //private Tile[][] tiles2D;
 
     public Map(int width, int height) {
-        // tiles = new ArrayList<>();
-        tiles2D = new Tile[10][10];
+        tiles = new ArrayList<>();
+        //tiles2D = new Tile[10][10];
         this.width = width;
         this.height = height;
     }
 
     public void init() {
-        for (int x = 0; x < tiles2D.length; x++) {
-            for (int y = 0; y < tiles2D[0].length; y++) {
-                tiles2D[x][y] = new Tile(x * Game.BLOCKSIZE * Game.SCALE, y * Game.BLOCKSIZE * Game.SCALE, Game.BLOCKSIZE * Game.SCALE, Game.BLOCKSIZE * Game.SCALE, new Ground());
+        for (int x = 0; x < width / 32; x++) {
+            for (int y = 0; y < height / 32; y++) {
+                tiles.add(new Tile(x * Game.BLOCKSIZE * Game.SCALE, y * Game.BLOCKSIZE * Game.SCALE, Game.BLOCKSIZE * Game.SCALE, Game.BLOCKSIZE * Game.SCALE, new Ground()));
             }
         }
         System.out.println("Grootte van map");
@@ -33,10 +34,8 @@ public class Map {
     }
 
     public void render(Graphics2D g) {
-        for (int x = 0; x < tiles2D.length; x++) {
-            for (int y = 0; y < tiles2D[0].length; y++) {
-                tiles2D[x][y].render(g);
-            }
-        }        
+        for (Tile tile : tiles) {
+            tile.render(g);
+        }
     }
 }
