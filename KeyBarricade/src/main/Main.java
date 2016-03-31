f7855e5bdf210817cb91910eb14ea7dd76a1800c
Matthias Aarnoutse <m.aarnoutse117@gmail.com>
2016-03-31 13:05:54 +0200

    Icon

--- KeyBarricade/src/main/Main.java
+++ KeyBarricade/src/main/Main.java
@@ -1,14 +1,23 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Main {
     
-    public static void main(String[] args) {
+    private static BufferedImage icon;
+    
+    public static void main(String[] args) throws IOException {
+        icon = ImageIO.read(new File("./assets/icon.png"));
+        
         JFrame frame = new JFrame();
 
         frame.setTitle("KeyBarricade");
         frame.setResizable(false);
+        frame.setIconImage(icon);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.add(new Game());
