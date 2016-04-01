705e6b610db26a6e5c8a37b781d16aa26d41d034
Matthias Aarnoutse <m.aarnoutse117@gmail.com>
2016-04-01 09:17:32 +0200

    HUD

---
+++ KeyBarricade/src/main/HUD.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.awt.Graphics2D;
+
+public class HUD {
+
+    public HUD() {
+
+    }
+
+    public void init() {
+        
+    }
+    
+    public void render(Graphics2D g) {
+        
+    }
+    
+    public void showMessage(String s) {
+        
+    }
+}
