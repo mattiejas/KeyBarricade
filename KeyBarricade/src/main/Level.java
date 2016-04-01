a55cdfffa2e52b2f74931a8971ff5b7cf4f2f473
Jeffrey <15036278@student.hhs.nl>
2016-04-01 10:39:30 +0200

    Changing level

--- KeyBarricade/src/main/Level.java
+++ KeyBarricade/src/main/Level.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Level {
 
-    private int[][] level;
-    private Difficulty difficulty;
+    private final int[][] LEVEL;
+    private Difficulty DIFFICULTY;
     private Random r;
     
     private final int GROUND = 0;
@@ -14,50 +14,83 @@ public class Level {
     private final int KEY = 3;    
 
     public Level(Difficulty diff) {
-        this.difficulty = diff;
-        this.level = new int[10][10];
+        this.DIFFICULTY = diff;
+        this.LEVEL = new int[10][10];
     }
 
     public void init() {
         r = new Random();
         
-        for (int i = 0; i < level.length; i++) {
-            for (int j = 0; j < level[i].length; j++) {
-                level[i][j] = -1;
+        // Insert the whole array with value -1 in order to track the 'empty' level tiles.
+        for (int i = 0; i < LEVEL.length; i++) {
+            for (int j = 0; j < LEVEL[i].length; j++) {
+                LEVEL[i][j] = -1;
             }
         }
         
-        switch (difficulty) {
+        switch (DIFFICULTY) {
             case EASY:
                 generateEasy();
                 break;
             case NORMAL:
-                generateEasy();
+                generateNormal();
                 break;
             case HARD:
-                generateEasy();
+                generateHard();
                 break;
             case IMPOSSIBLE:
-                generateEasy();
+                generateImpossible();
                 break;
         }
     }
 
     private void generateEasy() {
-        int wallCount = 0;
         int wallLimit = 30;
-        
-        int barricadeCount = 0;
         int barricadeLimit = 10;
         
-        for (int i = 0; i < level.length; i++) {
-            for (int j = 0; j < level[i].length; j++) {
-                int random = r.nextInt(2) + 1;
+        generateLevel(wallLimit, barricadeLimit);
+        generateKey(3);
+        generateGround();
+    }
 
-                int randomI = r.nextInt(10);
-                int randomJ = r.nextInt(10);
+    private void generateNormal() {
+        int wallLimit = 30;
+        int barricadeLimit = 20;
+        
+        generateLevel(wallLimit, barricadeLimit);
+        generateKey(3);
+        generateGround();
+    }
 
-                if (level[randomI][randomJ] == -1) {
+    private void generateHard() {
+        int wallLimit = 30;
+        int barricadeLimit = 30;
+        
+        generateLevel(wallLimit, barricadeLimit);
+        generateKey(3);
+        generateGround();
+    }
+
+    private void generateImpossible() {
+        LEVEL[0][1] = BARRICADE;
+        LEVEL[1][1] = WALL;
+        LEVEL[1][0] = WALL;
+        LEVEL[0][2] = KEY;
+        generateGround();
+    }
+    
+    private void generateLevel(int wallLimit, int barricadeLimit) {
+        int wallCount = 0;
+        int barricadeCount = 0;
+        
+        for (int i = 0; i < LEVEL.length; i++) {
+            for (int j = 0; j < LEVEL[i].length; j++) {
+                int random = r.nextInt(2) + 1; // Set random number between WALL and BARRICADEs
+
+                int randomY = r.nextInt(10);
+                int randomX = r.nextInt(10);
+
+                if (LEVEL[randomY][randomX] == -1) {
                     if (random == WALL) {
                         wallCount++;
                         if(wallCount > wallLimit) {
@@ -70,55 +103,49 @@ public class Level {
                             random = GROUND;
                         }
                     }                    
-                    level[randomI][randomJ] = random;
-                }
-            }
-        }
-        
-        int keyCount = 0;
-        int keyLimit = 3;
-        for (int i = 0; i < level.length; i++) {
-            for (int j = 0; j < level[i].length; j++) {
-                keyCount++;
-                int randomI = r.nextInt(10);
-                int randomJ = r.nextInt(10);
-                if (keyCount <= keyLimit) {
-                    level[randomI][0] = KEY;
-                }
-            }   
-        }
-        
-        for (int i = 0; i < level.length; i++) {
-            for (int j = 0; j < level[i].length; j++) {
-                if (level[i][j] == -1) {
-                    level[i][j] = GROUND;
+                    LEVEL[randomY][randomX] = random;
                 }
             }
         }        
-       
     }
-
-    private void generateNormal() {
-
+    
+    private void generateKey(int keyLimit) {
+        int keyCount = 0;
+        for (int i = 0; i < LEVEL.length; i++) {
+            for (int j = 0; j < LEVEL[i].length; j++) {
+                keyCount++;
+                if (keyCount == 1) {
+                    // Place to first key close to the user
+                    int randomY = r.nextInt(3);
+                    int randomX = r.nextInt(3);
+                    
+                    if (randomY == 0 && randomX == 0) {
+                        randomY = r.nextInt(3);
+                        randomX = r.nextInt(3);
+                    }
+                    LEVEL[randomY][randomX] = KEY;
+                } else {
+                    int randomY = r.nextInt(10);
+                    int randomX = r.nextInt(10);
+                    if (keyCount <= keyLimit) {
+                        LEVEL[randomY][randomX] = KEY;
+                    }
+                }
+            }   
+        }        
     }
-
-    private void generateHard() {
-
-    }
-
-    private void generateImpossible() {
-    }
-
-    private int replaceBlockType(int blockType) {
-        int random = r.nextInt(4);
-        if (random == blockType) {
-            return replaceBlockType(blockType);
-        } else {
-            return random;
-        }
+    
+    private void generateGround() {
+        for (int y = 0; y < LEVEL.length; y++) {
+            for (int x = 0; x < LEVEL[y].length; x++) {
+                if (LEVEL[y][x] == -1) {
+                    LEVEL[y][x] = GROUND;
+                }
+            }
+        }        
     }
     
     public int[][] getLevel() {
-        return level;
+        return LEVEL;
     }
 }
