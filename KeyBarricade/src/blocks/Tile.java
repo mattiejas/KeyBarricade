8971b2c2d85aaa58fdfdec35f9453adc31b9265f
Evermeij <ewoudvermeij@gmail.com>
2016-04-04 12:51:19 +0200

    Comments Tile :yen:

--- KeyBarricade/src/blocks/Tile.java
+++ KeyBarricade/src/blocks/Tile.java
@@ -12,6 +12,17 @@ public class Tile {
     private final BlockType BLOCK;
     private final int MARGIN;
 
+    /**
+     * This constructor receives multiple ints and one 
+     * BlockType as parameters. This sets the location, 
+     * size and type of BlockType.
+     * 
+     * @param x         Sets the location x
+     * @param y         Sets the location y
+     * @param width     Sets the width of the tile
+     * @param height    Sets the height of the tile
+     * @param block     Sets the BlockType of the tile
+     */
     public Tile(int x, int y, int width, int height, BlockType block) {
         this.X = x;
         this.Y = y;
@@ -21,6 +32,12 @@ public class Tile {
         this.MARGIN = 2;
     }
 
+    /**
+     * This method draws the Tile on the screen. It 
+     * receives a Graphics2D parameter to draw.
+     * 
+     * @param g     draws by the given instructions.
+     */
     public void render(Graphics2D g) {
         g.drawImage(BLOCK.getSprite(), X, Y, WIDTH, HEIGHT, null);
         
@@ -32,18 +49,41 @@ public class Tile {
         }
     }
 
+    /**
+     * This method returns the value of x of this Tile.
+     * x is a certain location on the Map.
+     * 
+     * @return      returns value of X
+     */
     public int getPositionX() {
         return this.X;
     }
 
+    /**
+     * This method returns the value of y of this Tile.
+     * y is a certain location on the Map.
+     * 
+     * @return      returns value of Y
+     */
     public int getPositionY() {
         return this.Y;
     }
-
+    
+    /**
+     * This method returns the value of the BLOCK.
+     * 
+     * @return      returns value of BLOCK. 
+     */
     public BlockType getBlockType() {
         return BLOCK;
     }
 
+    /**
+     * This method tells you whether the Block of this
+     * Tile is solid or not.
+     * 
+     * @return      returns the solid state of BLOCK 
+     */
     public boolean getSolid() {
         return BLOCK.getSolid();
     }
