9983c4c2a529d09452cf6354e39b4f02a32cdca4
Matthias Aarnoutse <m.aarnoutse117@gmail.com>
2016-04-04 12:27:05 +0200

    Main comments

--- KeyBarricade/src/main/Main.java
+++ KeyBarricade/src/main/Main.java
@@ -6,6 +6,20 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
+/**
+ * <h1> KeyBarricade </h1>
+ * KeyBarricade is a game about unlocking barricades 
+ * with keys. Keys and barricades could have different points.
+ * That means that you cannot always unlock a barricade with the key
+ * you are currently holding.
+ * 
+ * @author Matthias Aarnoutse
+ * @author Jeffrey Dufour
+ * @author Ewoud Vermeij
+ * 
+ * @version 1.0
+ */
+
 public class Main {
     
     private static BufferedImage icon;
