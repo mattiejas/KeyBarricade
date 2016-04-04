dbb866f607fa4054c24c8d9ef538e723b5030d1b
Evermeij <ewoudvermeij@gmail.com>
2016-04-04 12:41:13 +0200

    Comments Key :key:

--- KeyBarricade/src/blocks/Key.java
+++ KeyBarricade/src/blocks/Key.java
@@ -5,6 +5,12 @@ import assets.Sprite;
 
 public class Key extends BlockType {
 
+    /**
+     * This constructor receives an int as a parameter
+     * and sets the amount of points for this key.
+     * 
+     * @param points        sets the amount of points for this key
+     */
     public Key(int points) {
         this.image = ResourceLoader.getSprite(Sprite.KEY);
         this.points = points;
