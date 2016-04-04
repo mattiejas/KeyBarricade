c851059d30760ca05c7de302993378df5187e627
Evermeij <ewoudvermeij@gmail.com>
2016-04-04 12:55:23 +0200

    Comments Wall

--- KeyBarricade/src/blocks/Wall.java
+++ KeyBarricade/src/blocks/Wall.java
@@ -5,6 +5,10 @@ import assets.Sprite;
 
 public final class Wall extends BlockType {
 
+    /**
+     * This constructor of Wall sets the isSolid boolean
+     * to true. Also the image for Wall wil be set.
+     */
     public Wall() {
         this.isSolid = true;
         this.image = ResourceLoader.getSprite(Sprite.WALL);
