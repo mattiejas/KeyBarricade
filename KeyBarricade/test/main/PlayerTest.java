7419932489b032da995f35065a4654689b7174a5
Evermeij <ewoudvermeij@gmail.com>
2016-04-05 09:52:43 +0200

    playerTest

--- KeyBarricade/test/main/PlayerTest.java
+++ KeyBarricade/test/main/PlayerTest.java
@@ -5,7 +5,10 @@
  */
 package main;
 
+import assets.ResourceLoader;
+import com.sun.glass.events.KeyEvent;
 import java.awt.Graphics2D;
+import map.Map;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,50 +21,95 @@ import static org.junit.Assert.*;
  * @author Ewoud
  */
 public class PlayerTest {
-    
+
+    Map map;
+    HUD hud;
+
     public PlayerTest() {
+        ResourceLoader.init();
+        map = new Map(Difficulty.EASY, hud);
+        hud = new HUD();
+
+        map.init();
+
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
-    }
-
-    /**
-     * Test of render method, of class Player.
-     */
-    @Test
-    public void testRender() {
-        System.out.println("render");
-        Graphics2D g = null;
-        Player instance = null;
-        instance.render(g);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of keyPressed method, of class Player.
      */
     @Test
-    public void testKeyPressed() {
-        System.out.println("keyPressed");
-        int k = 0;
-        Player instance = null;
-        instance.keyPressed(k);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testKeyPressedRight() {
+        System.out.println("keyPressedRight");
+        Player instance = new Player(map, hud);
+
+        int k = KeyEvent.VK_RIGHT;
+        
+        if (!map.getTile(1, 0).getSolid()) {
+            instance.keyPressed(k);
+            int actualsX = instance.getPositionX();
+            int actualsY = instance.getPositionY();
+            int expectedsX = 1;
+            int expectedsY = 0;
+            
+            assertEquals(expectedsX, actualsX);
+            assertEquals(expectedsY, actualsY);
+        }
+        else{
+            int actualsX = instance.getPositionX();
+            int actualsY = instance.getPositionY();
+            int expectedsX = 0;
+            int expectedsY = 0;
+            
+            assertEquals(expectedsX, actualsX);
+            assertEquals(expectedsY, actualsY);
+        }
+    }
+    
+    /**
+     * Test of keyPressed method, of class Player.
+     */
+    @Test
+    public void testKeyPressedLeft() {
+        System.out.println("keyPressedLeft");
+        Player instance = new Player(map, hud);
+
+        int k = KeyEvent.VK_RIGHT;
+        
+        if (!map.getTile(1, 0).getSolid()) {
+            instance.keyPressed(k);
+            int actualsX = instance.getPositionX();
+            int actualsY = instance.getPositionY();
+            int expectedsX = 1;
+            int expectedsY = 0;
+            
+            assertEquals(expectedsX, actualsX);
+            assertEquals(expectedsY, actualsY);
+        }
+        else{
+            int actualsX = instance.getPositionX();
+            int actualsY = instance.getPositionY();
+            int expectedsX = 0;
+            int expectedsY = 0;
+            
+            assertEquals(expectedsX, actualsX);
+            assertEquals(expectedsY, actualsY);
+        }
     }
 
     /**
@@ -71,9 +119,8 @@ public class PlayerTest {
     public void testUseKey() {
         System.out.println("useKey");
         Player instance = null;
-        instance.useKey();
+
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -83,65 +130,8 @@ public class PlayerTest {
     public void testGrabKey() {
         System.out.println("grabKey");
         Player instance = null;
-        instance.grabKey();
+
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of getPositionX method, of class Player.
-     */
-    @Test
-    public void testGetPositionX() {
-        System.out.println("getPositionX");
-        Player instance = null;
-        int expResult = 0;
-        int result = instance.getPositionX();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPositionY method, of class Player.
-     */
-    @Test
-    public void testGetPositionY() {
-        System.out.println("getPositionY");
-        Player instance = null;
-        int expResult = 0;
-        int result = instance.getPositionY();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getArrayX method, of class Player.
-     */
-    @Test
-    public void testGetArrayX() {
-        System.out.println("getArrayX");
-        Player instance = null;
-        int expResult = 0;
-        int result = instance.getArrayX();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getArrayY method, of class Player.
-     */
-    @Test
-    public void testGetArrayY() {
-        System.out.println("getArrayY");
-        Player instance = null;
-        int expResult = 0;
-        int result = instance.getArrayY();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
