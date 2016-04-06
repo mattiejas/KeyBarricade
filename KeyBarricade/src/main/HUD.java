cc90d0a03f16b5d087f72e076011c810213e37b7
Matthias Aarnoutse <m.aarnoutse117@gmail.com>
2016-04-06 13:09:54 +0200

    Hud shit

--- KeyBarricade/src/main/HUD.java
+++ KeyBarricade/src/main/HUD.java
@@ -1,5 +1,7 @@
 package main;
 
+import assets.ResourceLoader;
+import assets.Sprite;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
@@ -39,14 +41,15 @@ public class HUD implements ActionListener {
         MESSAGE_TIMER = new Timer(4000, this);
         PRESS_KEY_TIMER = new Timer(700, this);
 
-        FONT = new Font[2];
+        FONT = new Font[3];
         MOTIVATIONAL_MESSAGES = new ArrayList<>();
     }
 
     public void init() {
         // Initialize fonts
         FONT[0] = new Font("Joystix Monospace", Font.PLAIN, 36);
-        FONT[1] = new Font("Joystix Monospace", Font.PLAIN, 18);
+        FONT[1] = new Font("Joystix Monospace", Font.PLAIN, 40);
+        FONT[2] = new Font("Joystix Monospace", Font.PLAIN, 28);
 
         // New colors
         blink = Color.WHITE;
@@ -68,12 +71,13 @@ public class HUD implements ActionListener {
         // If the game is finished draw this..
         if (winGame) {
             hasItem = false;
+            newMessage = false;
 
             g.setColor(new Color(0, 0, 0, 1f));
             g.fillRect(0, 0, Game.HORIZONTAL_AMOUNT * Game.BLOCK_SIZE, Game.VERTICAL_AMOUNT * Game.BLOCK_SIZE);
             g.setColor(new Color(0, 0, 0, 0.7f));
             g.fillRect((int) (Game.BLOCK_SIZE * 0.5), (int) (Game.BLOCK_SIZE * 0.5), Game.WINDOW_WIDTH - (1 * Game.BLOCK_SIZE), Game.WINDOW_HEIGHT - (1 * Game.BLOCK_SIZE));
-            g.setColor(Color.WHITE);
+            g.setColor(new Color(1, 1, 1, 0.5f));
             g.drawRect((int) (Game.BLOCK_SIZE * 0.5), (int) (Game.BLOCK_SIZE * 0.5), Game.WINDOW_WIDTH - (1 * Game.BLOCK_SIZE), Game.WINDOW_HEIGHT - (1 * Game.BLOCK_SIZE));
 
             g.setFont(FONT[0]);
@@ -83,14 +87,14 @@ public class HUD implements ActionListener {
             g.setColor(Color.WHITE);
             g.drawString("Congratulations!", (Game.WINDOW_WIDTH / 2) - (width / 2), 3 * Game.BLOCK_SIZE);
 
-            g.setFont(FONT[1]);
+            g.setFont(FONT[2]);
             width = g.getFontMetrics().stringWidth("You won the game");
             g.setColor(Color.BLACK);
-            g.drawString("You won the game", (Game.WINDOW_WIDTH / 2) - (width / 2), 3 * Game.BLOCK_SIZE + 22);
+            g.drawString("You won the game", (Game.WINDOW_WIDTH / 2) - (width / 2), 3 * Game.BLOCK_SIZE + 25);
             g.setColor(Color.WHITE);
-            g.drawString("You won the game", (Game.WINDOW_WIDTH / 2) - (width / 2), 3 * Game.BLOCK_SIZE + 20);
+            g.drawString("You won the game", (Game.WINDOW_WIDTH / 2) - (width / 2), 3 * Game.BLOCK_SIZE + 23);
 
-            g.setFont(FONT[1]);
+            g.setFont(FONT[2]);
             width = g.getFontMetrics().stringWidth("Press SPACE to continue..");
             PRESS_KEY_TIMER.start();
             g.setColor(blink);
@@ -99,21 +103,39 @@ public class HUD implements ActionListener {
 
         // If player picked up an item, draw this..
         if (hasItem) {
-            g.setFont(FONT[1]);
-            int width = g.getFontMetrics().stringWidth(itemName + ": " + itemCount);
 
-            g.setColor(Color.BLACK);
-            g.drawString(itemName + ": " + itemCount, Game.WINDOW_WIDTH - width - 10, 30);
+            g.setFont(FONT[1]);
+            int height = g.getFontMetrics().getHeight() + 10;
+            g.setFont(FONT[2]);
+            int width = g.getFontMetrics().stringWidth(itemCount + "") + Game.BLOCK_SIZE / 2;
+
+            g.setColor(new Color(0, 0, 0, 0.5f));
+            g.fillRect(Game.WINDOW_WIDTH - (width * 2 + 5), 0, width * 2 - 20, height + 10);
+            g.setColor(new Color(1, 1, 1, 0.5f));
+            g.drawRect(Game.WINDOW_WIDTH - (width * 2 + 5), -1, width * 2 - 20, height + 10);
+
+            g.setColor(new Color(0, 0, 0, 0.5f));
+            g.drawString(itemCount + "", Game.WINDOW_WIDTH - width + 2, height + 3);
             g.setColor(Color.WHITE);
-            g.drawString(itemName + ": " + itemCount, Game.WINDOW_WIDTH - width - 10, 28);
+            g.drawString(itemCount + "", Game.WINDOW_WIDTH - width, height);
+            //g.drawImage(ResourceLoader.getSprite(Sprite.ITEM_KEY), Game.WINDOW_WIDTH - Game.BLOCK_SIZE * 2, 0, Game.BLOCK_SIZE, Game.BLOCK_SIZE, null);
+
+            g.setFont(FONT[1]);
+            g.setColor(new Color(0, 0, 0, 0.5f));
+            width = width + g.getFontMetrics().stringWidth(itemName);
+            g.drawString(itemName, Game.WINDOW_WIDTH - width + 3, height + 3);
+//            g.setColor(Color.YELLOW);
+//            g.drawString(itemName, Game.WINDOW_WIDTH - Game.BLOCK_SIZE * 2 - 3, g.getFontMetrics().getHeight() + 11);
+            g.setColor(Color.WHITE);
+            g.drawString(itemName, Game.WINDOW_WIDTH - width, height);
         }
 
         // If there is a message ready to be drawn, do it..
         if (newMessage) {
             g.setFont(FONT[1]);
 
-            g.setColor(Color.BLACK);
-            g.drawString(message, 21, Game.WINDOW_HEIGHT - 28);
+            g.setColor(new Color(0, 0, 0, 0.5f));
+            g.drawString(message, 23, Game.WINDOW_HEIGHT - 27);
 
             g.setColor(Color.WHITE);
             g.drawString(message, 20, Game.WINDOW_HEIGHT - 30);
@@ -142,10 +164,10 @@ public class HUD implements ActionListener {
 
     /**
      * Sets the item in the upper right corner of the screen.
-     *     
-     * @param itemName  the name of the item
-     * @param count     amount of items
-    */
+     *
+     * @param itemName the name of the item
+     * @param count amount of items
+     */
     public void setItem(String itemName, int count) {
         this.hasItem = true;
         this.itemName = itemName;
@@ -192,6 +214,7 @@ public class HUD implements ActionListener {
 
     /**
      * Checks whether the game is ready to start a new game.
+     *
      * @return <code>true</code> if ready, otherwise <code>false</code>
      */
     public boolean isReady() {
